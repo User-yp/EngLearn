c02114648143fc8e6c6c3db9e74431a0379c61a4
鹏叶 <1277206411yp@gmail.com>
2024-03-12 16:01:22 +0800

    http/ip

---
+++ SearchService.Infrastructure/ModuleInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Commons;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Nest;
+using SearchService.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchService.Infrastructure;
+
+internal class ModuleInitializer : IModuleInitializer
+{
+    public void Initialize(IServiceCollection services)
+    {
+        services.AddHttpClient();
+        services.AddScoped<IElasticClient>(sp =>
+        {
+            var option = sp.GetRequiredService<IOptions<ElasticSearchOptions>>();
+            var settings = new ConnectionSettings(option.Value.Url);
+            return new ElasticClient(settings);
+        });
+        services.AddScoped<ISearchRepository, SearchRepository>();
+    }
+}
